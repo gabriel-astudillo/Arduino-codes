3e48d1dc5acc3242c47bc022bad298f60307a451
Gabriel Astudillo <gabriel.astudillo@gmail.com>
2017-11-24 15:54:51 -0300

    Update conversion.c

--- morse/conversion.c
+++ morse/conversion.c
@@ -3,3 +3,54 @@ char* morseLetras[] = {
 ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", // j-r 
 "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." // s-z
 };
+
+int retardoPunto = 200;
+int retardoRaya  = retardoPunto*3;
+
+void setup() {
+  //Configurar pines de salida
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  //Completar código adecuadamente
+  char ch;
+  String ch_morse;
+  
+  int ch_valor;
+  
+  if (Serial.available()){
+    ch = Serial.read();
+
+    ch_valor = ch;
+    
+    Serial.print(ch);
+    Serial.print(" ");
+    if(ch_valor == 32){
+      Serial.println("Espacio");
+    }
+    else{
+      ch_morse = morseLetras[ch_valor - 97];
+      Serial.println(ch_morse);
+    }
+
+   
+  }
+}
+
+
+void secuencia(char* secuenciaMorse){
+  //Completar código adecuadamente
+
+
+}
+
+void parpadeoPuntoRaya(char puntoRaya){
+  //Completar código adecuadamente
+}
+
+void parpadeoEspacio(){
+  //Completar código adecuadamente
+}
